63eaee169868a88edd12fef8860da07ec9d10b79
Erik Lupander <erik.lupander@callistaenterprise.se>
2017-01-14 16:20:27 +0100

    Added simple healthchecker to get rid of depending on curl in container

---
+++ healthchecker/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"flag"
+	"net/http"
+	"os"
+)
+
+func main() {
+	port := flag.String("port", "80", "port on localhost to check")
+	flag.Parse()
+
+	resp, err := http.Get("http://127.0.0.1:" + *port + "/health")
+	if err != nil {
+		os.Exit(1)
+	}
+	if resp.StatusCode != 200 {
+		os.Exit(1)
+	}
+	os.Exit(0)
+}
